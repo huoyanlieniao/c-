2ca708ed7931e6e3171ed2fca9467d4784211136
huoyanlieniao <huoyanlieniao@163.com>
2019-09-27 19:02:20 +0800

    Gold coin
    金币原始代码
    包含鼠标点击、得到当前数目、增加、删除

---
+++ 金币原始/金币.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class 金币 : MonoBehaviour
+{
+    public GameObject GO;
+    public GUIText guit;
+    public static int jin = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GO.GetComponent<Text>().text = "1000";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Click();
+        }
+     }
+    public string Add(int x)
+    {
+        jin = jin + x;
+        string b = jin.ToString();
+        return b;
+    }
+    public string Reduce(int x)
+    {
+        jin = jin - x;
+        string b = jin.ToString();
+        return b;
+    }
+    public void Click()
+    {
+            string c;
+            c = Add(5);
+            GO.GetComponent<Text>().text = c;   
+    }
+    public bool Judge()
+    {
+        if (jin > 0)
+        {
+            return true;
+        }
+        else return false;
+    }
+   public int Getnum()
+    {
+        return jin;
+    }
+
+
+}
